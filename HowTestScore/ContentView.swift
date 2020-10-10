6c2cb58dc1856772f8b61b523d60d5897c691028
Sakamoto Kousuke <ginkin35@gmail.com>
2020-10-10 11:29:25 +0900

    Initial Commit

---
+++ HowTestScore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HowTestScore
+//
+//  Created by 坂本弘輔 on 2020/10/10.
+//  Copyright © 2020 坂本弘輔. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
